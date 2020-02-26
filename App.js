a5c3b3bd2427dacd4327e4cfefcd8736c740dd8e
rudalson <rudalson@gmail.com>
2020-02-26 20:29:32 +0900

    first commit
    입력창 과 axios 를 통한 영화사이트 api 호출. 그리고 스크롤뷰로 결과물 보여주기

--- App.js
+++ App.js
@@ -6,16 +6,48 @@
  * @flow
  */
 
-import React from 'react';
-
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+import axios from 'axios';
+import {View, StyleSheet, Text, TextInput, ScrollView} from 'react-native';
 
 const App: () => React$Node = () => {
+  const apiurl = 'http://www.omdbapi.com/?apikey=dfe6d885';
+  const [state, setState] = useState({
+    s: 'Enter a movie...',
+    results: [],
+    selected: {},
+  });
+
+  const search = () => {
+    axios(apiurl + '&s=' + state.s).then(({data}) => {
+      let results = data.Search;
+
+      setState(prevState => {
+        return {...prevState, results: results};
+      });
+    });
+  };
+
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <Text>Hello World</Text>
-      </SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.title}>Movie DB</Text>
+        <TextInput
+          style={styles.searchbox}
+          onChangeText={text =>
+            setState(prevState => {
+              return {...prevState, s: text}
+          })}
+          onSubmitEditing={search}
+          value={state.s} />
+        <ScrollView style={styles.results}>
+          {state.results.map(result => (
+            <View key={result.imdbID} style={styles.result}>
+              <Text style={styles.heading}>{result.Title}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
     </>
   );
 };
@@ -23,9 +55,42 @@ const App: () => React$Node = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#223343',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 70,
+    paddingHorizontal: 20,
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 32,
+    fontWeight: '700',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  searchbox: {
+    fontSize: 20,
+    fontWeight: '300',
+    padding: 20,
+    width: '100%',
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    marginBottom: 40,
+  },
+  results: {
+    flex: 1,
+  },
+  result: {
+    flex: 1,
+    width: '100%',
+    marginBottom: 20,
+  },
+  heading: {
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: '700',
+    padding: 20,
+    backgroundColor: '#445565',
   },
 });
 
